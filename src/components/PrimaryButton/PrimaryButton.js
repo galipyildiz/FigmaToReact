79401382bdab89f3100b959c59d431c3e88f645d
galipyildiz <galipyildiz9606@outlook.com>
2024-04-28 03:17:58 +0300

    add: primary button component

--- src/components/PrimaryButton/PrimaryButton.js
+++ src/components/PrimaryButton/PrimaryButton.js
@@ -1,7 +1,8 @@
+import React from "react";
 import "./PrimaryButton.css";
 
-function PrimaryButton({ text }) {
-  return <button>{text}</button>;
+function PrimaryButton({ text, handleClick }) {
+  return <button className="primary-button" onClick={handleClick}>{text}</button>;
 }
 
 export default PrimaryButton;
