5af61cdacabff861236e182aad95edbbbd7b7ca1
galipyildiz <galipyildiz9606@outlook.com>
2024-04-28 03:32:48 +0300

    add: button click navigate to home page

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -2,17 +2,30 @@ import React from "react";
 import "./Welcome.css";
 import logo from "../../resources/logo.png";
 import PrimaryButton from "../../components/PrimaryButton";
+import { useNavigate } from "react-router-dom";
 
 function Welcome() {
+  const navigate = useNavigate();
+
+  const handleLoginClick = (e) => {
+    navigate("/home");
+  };
+
   return (
     <div className="background">
       <div className="background-image"></div>
       <div className="login-div">
         <img src={logo} alt="logo" height={32} />
         <p className="enjoy-text">Enjoy the newest movies</p>
-        <PrimaryButton handleClick={(e) => console.log(e)} text={"Log in"} />
+        <PrimaryButton
+          handleClick={(e) => handleLoginClick(e)}
+          text={"Log in"}
+        />
         <p>
-          No account? <a className="sign-up-link" href="#">Sign up</a>
+          No account?{" "}
+          <a className="sign-up-link" href="#">
+            Sign up
+          </a>
         </p>
       </div>
     </div>
