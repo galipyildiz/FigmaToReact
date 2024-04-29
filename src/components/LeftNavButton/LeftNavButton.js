51d84a11d57aee906296f254ac7b7128edd5d870
galipyildiz <galipyildiz9606@outlook.com>
2024-04-29 04:16:56 +0300

    add: leftnavbutton component

---
+++ src/components/LeftNavButton/LeftNavButton.js
@@ -0,0 +1,16 @@
+import "./LeftNavButton.css";
+
+function LeftNavButton({ text, icon, isActive }) {
+  return (
+    <button className={`nav-button ${isActive ? "nav-button-active" : ""}`}>
+      <img
+        className={!isActive ? "nav-button-icon" : ""}
+        src={icon}
+        height={24}
+      />
+      <span className="nav-button-text">{text}</span>
+    </button>
+  );
+}
+
+export default LeftNavButton;
